c0ff695799a4de67dc8751dc082f588c0901b0e9
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-15 13:22:11 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,63 +1,17 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
-let fs = require('fs')
-let handler  = async (m, { conn, usedPrefix }) => { 
-let str = `
-╭─˗ˏˋ *Informasi Owner* ´ˎ˗
-│ (Follow ig saya dong)
-│ ✎ Nama : Lui (cowok)
-│    Kelas : 9 SMP
-│    Umur : 15
-│    Asal : jawa
-│    Kabupaten: ponorogo
-│    Status : Privasi
-│    Suka : recode sc orang :v
-│ ✎ _Official Grup_ :
-│    https://chat.whatsapp.com/DdMbKYRxn5TCFrB9m10XnQ
-│ ✎ _Instagram_ :
-│    instagram.com/ahmdlui
-│ ✎ _github_ : 
-│    github.com/luigmntng
-│ ✎ _FaceBook_ :
-│    (Private)
-│ ✎ _WhatsApp_ :
-│    bit.ly/
-╰‿‿‿‿‿‿‿‿
-`.trim()
-const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-    templateMessage: {
-        hydratedTemplate: {
-          hydratedContentText: str,
-          locationMessage: { 
-          jpegThumbnail: fs.readFileSync('./media/shiraori.jpg') },           
-          hydratedFooterText: wm,
-          hydratedButtons: [{
-            urlButton: {
-              displayText: 'Official instagran',
-              url: 'https://instagram.com/ahmdlui'
-            }
-
-          },
-              {
-            quickReplyButton: {
-              displayText: 'Back To Menu',
-              id: '.menu',
-            }
-
-          }]
-        }
-      }
-    }), { userJid: m.sender, quoted: m });
-   //conn.reply(m.chat, text.trim(), m)
-   return await conn.relayMessage(
-        m.chat,
-        template.message,
-        { messageId: template.key.id }
-    )
+const PhoneNumber = require('awesome-phonenumber')
+async function handler(m) {
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+                    + 'VERSION:3.0\n' 
+                    + 'N:;Mas Lui;;;'
+                    + 'FN:Maszz Lui\n' // full name
+                    + 'ORG:WM SUKI BADAK;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6282146092695:+62 821-4609-2695\n' // WhatsApp ID + phone number
+                    + 'END:VCARD'
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Maszz Lui', contacts: [{ vcard }] } }, { quoted: m })
 }
-
-handler.help = ['owner'. 'creator']
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
-handler.command = /^(owner)$/i
 
+handler.command = /^(owner|creator)$/i
 
 module.exports = handler
