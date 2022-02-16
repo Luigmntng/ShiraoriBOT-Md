d4b37f3bcd6e7d4037ca937684ee6ea8ca2f22d7
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-16 21:59:21 +0700

    Update cuaca.js

--- plugins/cuaca.js
+++ plugins/cuaca.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn }) => {
   if (!json.url) throw 'Error!'
   conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
 }
-handler.help = ['megumin']
+handler.help = ['neko']
 handler.tags = ['anime']
-handler.command = /^(megumin)$/i
+handler.command = /^(neko)$/i
 
 handler.limit = true
 
