c5eaa3403b76f41222dc5756cc6de942559949cc
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-17 00:04:32 +0700

    Update quoteislam.js

--- plugins/quoteislam.js
+++ plugins/quoteislam.js
@@ -1,12 +1,12 @@
 let fetch = require('node-fetch')
  let handler = async (m, { conn, usedPrefix, command }) => {
  	let heum = await fetch(`https://api.lolhuman.xyz/api/quotes/islami?apikey=rey2k21`)
- amin = await heum.json()
-    conn.reply(m.chat, `${amin.result}`. m)
+ amin = await result.json()
+    conn.reply(m.chat, `${amin.result}`, m)
 
     }
     
 handler.help = ['quotesmuslim']
 handler.tags = ['islam']
-handler.command = /^(qmuslim)$/i
+handler.command = /^(quotesislam)$/i
 module.exports = handler
