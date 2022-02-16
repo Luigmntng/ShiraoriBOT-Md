83c65726dfa72d775a84dbad41f60071559045e4
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-17 00:33:13 +0700

    Update quoteislam.js

--- plugins/quoteislam.js
+++ plugins/quoteislam.js
@@ -2,9 +2,9 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
   await m.reply(global.wait)
   let res = await fetch('https://islamic-api-indonesia.herokuapp.com/api/data/quotes')
-  let son = await res.json()
-  shyi = son.data
-  let json = shyi[Math.floor(Math.random() * shyi.length)]
+  let son = await url.json()
+  shyi = son.url
+  let json = shyi[Math.floor(Math.random() * shyi.url)]
   let caption = `
 *「 Quotes Islam 」*
 ${json.text_en}
